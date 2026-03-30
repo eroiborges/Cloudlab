e68f3e7f3a7bdac21aa4cfcb54689a2214281387
Eroilton Borges <github@eroi.eti.br>
2026-03-30 17:51:35 -0300

    fix tag separator

--- AzureARC/extension-add.sh
+++ AzureARC/extension-add.sh
@@ -6,7 +6,7 @@ Publisher="Microsoft.AzureData"
 Target_Version="1.1.3348.364" #Source: https://github.com/MicrosoftDocs/sql-docs/blob/live/docs/sql-server/azure-arc/release-notes.md 
 resource_group="rg-arcservers" # Update with your resource group name
 serverlist='["server01","server02"]' #Update with your server names
-tags="env=prod;dept=sql" # Update with your desired tags
+tags="env=prod dept=sql" # Update with your desired tags using space-separated key=value pairs
 
 for server in $(echo $serverlist | jq -r '.[]'); do   
   check=$(az connectedmachine show --machine-name $server --resource-group $resource_group -o tsv --query id 2>/dev/null)
